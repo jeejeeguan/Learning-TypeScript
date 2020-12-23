ddd8beea8555952d83294759bade962be109ac2d
JeeJee <30567623+jeejeekwan@users.noreply.github.com>
2020-12-24 01:14:59 +0800

    tsconfig.json02

---
+++ demo16.ts
@@ -0,0 +1,9 @@
+// tsconfig.json
+
+// "noImplicitAny": true,
+/* Raise error on expressions and declarations with an implied 'any' type. */
+// 当类型注解为 any 时，必须标明该类型注解，否则会报错
+
+// "strictNullChecks": true,
+/* Enable strict null checks. */
+// 严格执行 null 值检查，如果有 null 值出现则会报错
